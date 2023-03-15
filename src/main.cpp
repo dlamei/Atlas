79b3c44b316e094757462756f225bef4d012d67e
kingalidj <44316365+Kingalidj@users.noreply.github.com>
2023-03-15 22:08:58 +0100

    added more customizations

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@
 using namespace Atlas;
 
 struct Agent {
-	glm::ivec2 speciesMask;
+	glm::ivec4 speciesMask;
 	glm::vec2 pos;
 	float angle;
 	int index;
@@ -36,7 +36,7 @@ struct GlobalSettings {
 	BlurSettings blur;
 };
 
-class Sandbox : public Atlas::Layer {
+class SimulationLayer : public Atlas::Layer {
 
 	Buffer agents;
 	Texture2D img;
@@ -45,11 +45,17 @@ class Sandbox : public Atlas::Layer {
 
 	GlobalSettings settings;
 
-	const uint32_t agentCount = 50000;
+	int agentCount = 50000;
+	int nSpecies = Random::get<int>(1, 3);
+	int imgSize = 128 * 5;
+	Atlas::TextureFilter filter = Atlas::TextureFilter::NEAREST;
 
 	OrthographicCameraController controller = OrthographicCameraController();
 
 	void reset_settings() {
+		//3
+		//2
+		//4
 		settings.sim.moveSpeed = 0.5f; //1
 		settings.sim.turnSpeed = 0.1f; //1.369
 		settings.sim.sensorAngle = 0.2f; //0.39
@@ -63,54 +69,53 @@ class Sandbox : public Atlas::Layer {
 		settings.blur.kernelSize = 1;
 	}
 
-	void on_attach() override {
-		controller.set_camera(0, 1, 0, 1);
-		Render2D::init();
-
-		const uint32_t imgSize = 128 * 5;
-		img = Texture2D::rgba(imgSize, imgSize, TextureFilter::NEAREST);
+	void init_sim() {
+		img = Texture2D::rgba(imgSize, imgSize, filter);
 
 		std::vector<Agent> agentsData(agentCount, Agent{});
+
 		for (auto &agent : agentsData) {
 			//agent.pos = Random::get<glm::vec2>(0, imgSize);
 			agent.pos = glm::vec2(imgSize / 2, imgSize / 2);
 			agent.angle = Random::get<float>(0.f, 2.f * PI);
 			agent.index = 0;
 
-			int species = Random::get<int>(0, 1);
+			int species = Random::get<int>(0, nSpecies - 1);
 			agent.speciesMask[species] = 1;
 		}
 
 		agents = Buffer::create(BufferType::STORAGE, agentsData.data(), agentsData.size() * sizeof(Agent));
-		agentShader = Shader::load_comp("assets/shaders/agents.comp");
-		blurShader = Shader::load_comp("assets/shaders/blur.comp");
 
 		agentShader.bind("Agents", agents);
 		agentShader.bind("outImg", img, TextureUsage::WRITE);
 
 		blurShader.bind("img", img, TextureUsage::READ | TextureUsage::WRITE);
 
+
+		Buffer sim = Buffer::storage(settings.sim);
+		agentShader.bind("settingsBuffer", sim);
+
+		Buffer blur = Buffer::storage(settings.blur);
+		blurShader.bind("settingsBuffer", blur);
+	}
+
+	void on_attach() override {
+		controller.set_camera(0, 1, 0, 1);
+		Render2D::init();
+
+		agentShader = Shader::load_comp("assets/shaders/agents.comp");
+		blurShader = Shader::load_comp("assets/shaders/blur.comp");
+
 		reset_settings();
-
-		{
-			Buffer sim = Buffer::storage(settings.sim);
-			agentShader.bind("settingsBuffer", sim);
-
-			Buffer blur = Buffer::storage(settings.blur);
-			blurShader.bind("settingsBuffer", blur);
-		}
+		init_sim();
 	}
 
 	void on_update(Timestep ts) override {
 
 
-		Shader::dispatch(blurShader, img.width() / 32, img.height() / 32, 1);
-		memory_barrier(Barrier::IMAGE_ACCESS);
 
 		Shader::dispatch(agentShader, (agentCount + 1023) / 1024, 1, 1);
-		memory_barrier(Barrier::ALL);
-
-		bool updated = true;
+		Shader::dispatch(blurShader, (img.width() + 31) / 32, img.height() / 32, 1);
 
 		controller.on_update(ts);
 		Render2D::set_camera(controller.get_camera());
@@ -136,6 +141,26 @@ class Sandbox : public Atlas::Layer {
 
 
 		ImGui::Begin("Settings");
+
+		ImGui::Text("Simulation");
+		ImGui::DragInt("resolution", &imgSize, 32, 0, 3840);
+		ImGui::DragInt("agents", &agentCount, 128, 0, 100000000);
+		ImGui::DragInt("species", &nSpecies, 1, 1, 3);
+
+		const char *filterName = "NONE";
+		if (filter == TextureFilter::LINEAR) filterName = "linear";
+		else if (filter == TextureFilter::NEAREST) filterName = "nearest";
+
+		ImGui::Text("filter ");
+		ImGui::SameLine();
+		if (ImGui::Button(filterName)) {
+			if (filter == TextureFilter::LINEAR) filter = TextureFilter::NEAREST;
+			else if (filter == TextureFilter::NEAREST) filter = TextureFilter::LINEAR;
+		}
+
+		if (ImGui::Button("reset simulation")) {
+			init_sim();
+		}
 
 		ImGui::Text("Agents");
 		ImGui::DragFloat("move speed", &simSettings.moveSpeed, 0.001, 0, 5);
@@ -196,8 +221,7 @@ class Sandbox : public Atlas::Layer {
 
 int main() {
 	Atlas::Application app = Atlas::Application::default();
-
-	app.push_layer(make_ref<Sandbox>());
+	app.push_layer(make_ref<SimulationLayer>());
 	app.run();
 
 }
